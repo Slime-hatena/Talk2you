04892a157b482ff37a0b3b4350cf1ae65904a884
Slime_hatena <slime.hatena@gmail.com>
2016-12-04 02:56:55 +0900

    dataGridの編集機能を仮追加

--- Talk2you/ViewModels/MainWindowViewModel.cs
+++ Talk2you/ViewModels/MainWindowViewModel.cs
@@ -62,7 +62,6 @@ namespace Talk2you.ViewModels
          * 自動的にUIDispatcher上での通知に変換されます。変更通知に際してUIDispatcherを操作する必要はありません。
          */
 
-        public MainWindowViewModel vm = new MainWindowViewModel();
         public VoicePlayer voicePlayer = new VoicePlayer();
         public ProjectManager projectManager = new ProjectManager();
         public MainWindow ViewSource = (MainWindow)Application.Current.MainWindow;
@@ -73,42 +72,6 @@ namespace Talk2you.ViewModels
 
         // 時間を切り捨てる際 少数何桁で切り捨てるか
         private const int kRoundBy = 3;
-
-        /// <summary>
-        /// １つのセリフ情報を保存するクラス
-        /// </summary>
-        public class WordInformationViewModel
-        {
-            public WordInformation Item { get; set; }
-
-            /// <summary>
-            /// データグリッドのコンテキストメニューの再生をおした時
-            /// </summary>
-            public void DataGridClickPlay(WordInformation data)
-            {
-                Console.WriteLine("てすと");
-                vm.projectManager.GetAllWordInfomation();
-
-            }
-
-            /// <summary>
-            /// dataGridに登録されている項目を編集出来るよう各変数にセットし直す
-            /// </summary>
-            public void DataGridEdit(WordInformation data)
-            {
-
-                Console.WriteLine(data);
-                Console.WriteLine("てすと");
-            }
-
-            /// <summary>
-            /// dataGridに登録されている項目を削除する
-            /// </summary>
-            public void DataGridDelete()
-            {
-                Console.WriteLine("てすと");
-            }
-        }
 
         #region List変更通知プロパティ
         private ObservableCollection<WordInformationViewModel> _List = new ObservableCollection<WordInformationViewModel>();
---
+++ Talk2you/ViewModels/WordInformationViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using Talk2you.Models;
+using Talk2you.Views;
+using System.Windows;
+using System.Collections.ObjectModel;
+
+namespace Talk2you.ViewModels
+{
+    /// <summary>
+    /// １つのセリフ情報を保存するクラス
+    /// </summary>
+    public class WordInformationViewModel : ViewModel
+    {
+        public MainWindowViewModel vm = new MainWindowViewModel();
+        public VoicePlayer voicePlayer = new VoicePlayer();
+        public ProjectManager projectManager = new ProjectManager();
+        public MainWindow ViewSource = (MainWindow)Application.Current.MainWindow;
+
+        public WordInformation Item { get; set; }
+
+
+        /// <summary>
+        /// データグリッドのコンテキストメニューの再生をおした時
+        /// </summary>
+        public void DataGridClickPlay()
+        {
+
+        }
+
+        /// <summary>
+        /// dataGridに登録されている項目を編集出来るよう各変数にセットし直す
+        /// </summary>
+        public void DataGridClickEdit()
+        {
+            vm.Identifier = Item.Identifier;
+            vm.VoiceText = Item.Text;
+            vm.Category = Item.Category;
+            vm.VolumeChange = Item.Volume;
+            vm.StartTime = Item.Start;
+            vm.EndTime = Item.End;
+            vm.VoiceFile = Item.File;
+
+            //todo 12/4 何故かviewに反映されない
+        }
+
+        /// <summary>
+        /// dataGridに登録されている項目を削除する
+        /// </summary>
+        public void DataGridClickDelete()
+        {
+            Console.WriteLine("てすと");
+        }
+    }
+
+}
+
